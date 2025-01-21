25cac26629a8d6a69d80557383016cb69a80e194
gt124578 <97751719+gt124578@users.noreply.github.com>
2025-01-21 16:05:25 +0100

    Update EDP_1v5.py

--- EDP_1v5.py
+++ EDP_1v5.py
@@ -12,6 +12,8 @@ nx = 100     # Nombre de points en espace
 dt = 0.01    # Nombre de points en temps
 D = 0.01     # Diffusivité thermique
 C = 3   # Coefficient pour le terme advection
+a=0.3
+b=0.6
 dx = L / (nx - 1)   # Discrétisation en espace
 nt = int(T / dt)    # Discrétisation en temps
 u = np.zeros(nx)    # Initialisation de la température
@@ -29,16 +31,22 @@ u_mat = np.zeros((nt, nx))
 u_mat[0, :] = u
 
 # Définition de l'exacte solution u_exact
+# def u_exact(x, t, D=D):
+#     return sp.sin(sp.pi * x)*(1 + t) 
+
 def u_exact(x, t, D=D):
-    return np.sin(np.pi * x) * (1 + t) 
+    return (1/np.sqrt(2*np.pi))*sp.exp((-x**2)/2)*(1+t)
 
 
 x, t = sp.symbols('x t')
-u=sp.sin(sp.pi * x) * (1 + t) 
-du_dx = sp.diff(u, x)
+u_ex=(1/np.sqrt(2*np.pi))*sp.exp((-x**2)/2)*(1+t)
+du_dx = sp.diff(u_ex, x)
 du_dx_2=sp.diff(du_dx,x)
-du_dt = sp.diff(u, t)
+du_dt = sp.diff(u_ex, t)
 f=du_dt-D*du_dx_2+C*du_dx
+
+def fonction_f(n,L=L,f=f):
+    return float(f.subs({x: L, t: n})*10)
 
 
 # Boucle temporelle
@@ -46,7 +54,7 @@ for n in range(0, nt):
     u_new = u.copy()
     for i in range(0, nx - 1):
         # Calcul avec ajout de F(x) dans l'équation
-        u_new[i] = u[i] + D * dt / dx**2 * (u[i + 1] - 2 * u[i] + u[i - 1]) + C * dt / dx * (u[i + 1] - u[i]) + float(f.subs({x: L, t: n}))*dt # Ajout de la source F(x) ici
+        u_new[i] = u[i] + D * dt / dx**2 * (u[i + 1] - 2 * u[i] + u[i - 1]) + C * dt / dx * (u[i + 1] - u[i]) + fonction_f(n)*dt # Ajout de la source F(x) ici
     u = u_new.copy()
     u_mat[n, :] = u
 
