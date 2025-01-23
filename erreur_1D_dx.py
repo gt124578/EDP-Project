2b1a31715d32685a470bfe789f2b125c82fafb8d
gt124578 <97751719+gt124578@users.noreply.github.com>
2025-01-23 22:07:19 +0100

    Update erreur_1D_dx.py

--- erreur_1D_dx.py
+++ erreur_1D_dx.py
@@ -1,12 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy as sp
 
-# Paramètres
-def function_f(x, t, D, C):
-    """Terme source."""
-    return np.sin(np.pi * x) + D * (np.pi**2) * np.sin(np.pi * x) * (1 + t) + C * np.pi * np.cos(np.pi * x) * (1 + t)
+# Différentes valeurs de nx
+nx_values = [50, 100, 200, 400]  # Résolutions spatiales
+L = 1.0     # Longueur de la barre
+T = 2.0     # Temps total
+D = 0.01    # Coefficient de diffusion
+C = 0.03    # Coefficient de convection
 
-def u_exact(x, t, D):
+
+#Permet de calculer f(x,t) pour u quelconque par le calcul formel, il suffit de modifier u_ex dans la fonction
+def determinate_f():
+    y, u = sp.symbols('y u') #les variables sont y et t pour la résolution formelle 
+    #u_ex=sp.sin(np.pi*y)*(1+t)
+    u_ex = (1/np.sqrt(2*np.pi))*sp.exp(-(y**2)/2)*(1+u) #gaussienne avec mu=0 et sigma=1
+    du_dx = sp.diff(u_ex, y)
+    du_dx_2 = sp.diff(du_dx, y)
+    du_dt = sp.diff(u_ex, u)
+    f = du_dt - D * du_dx_2 + C * du_dx
+    print(f)
+    return f
+
+y, u = sp.symbols('y u')
+f=determinate_f()
+
+def function_f(x, tps, know_f=True, f=f, D=D, C=C):
+    if know_f:
+        """Terme source."""
+        return np.sin(np.pi * x) + D * (np.pi**2) * np.sin(np.pi * x) * (1 + tps) + C * np.pi * np.cos(np.pi * x) * (1 + tps)
+    else:
+        return float(f.subs({y: x, u: tps}))
+
+
+def u_exact(x, t):
     return np.sin(np.pi * x) * (1 + t)
 
 def apply_boundary_conditions(u_n):
@@ -15,12 +42,7 @@ def apply_boundary_conditions(u_n):
     u_n[-1] = 0  # Dirichlet à droite
     return u_n
 
-# Différentes valeurs de nx
-nx_values = [50, 100, 200, 400]  # Résolutions spatiales
-L = 1.0     # Longueur de la barre
-T = 2.0     # Temps total
-D = 0.01    # Coefficient de diffusion
-C = 0.03    # Coefficient de convection
+
 
 def solve_heat_eq(nx, L, T, D, C):
     dx = L / (nx - 1)
@@ -40,7 +62,7 @@ def solve_heat_eq(nx, L, T, D, C):
         u[n + 1, :] = apply_boundary_conditions(u[n + 1, :])
 
     t = np.linspace(0, T, nt)
-    u_exact_mat = np.array([[u_exact(x[i], t[n], D) for i in range(nx)] for n in range(nt)])
+    u_exact_mat = np.array([[u_exact(x[i], t[n]) for i in range(nx)] for n in range(nt)])
     L2_error = np.sqrt(np.sum((u - u_exact_mat)**2, axis=1) * dx)  # Erreur en norme L2 pour chaque instant
     return x, t, L2_error
 
